2d7e4c942974773c97a0a9f0b394f2d387f8dad5
tmmets29-hash <tmmets29@gmail.com>
2026-03-07 22:10:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from flask import Flask, render_template, jsonify
+from scanner import scan_weather
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/scan")
+def scan():
+    return jsonify(scan_weather())
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
